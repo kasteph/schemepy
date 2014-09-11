73d122dfb5bb58759b0792ac9fd61f602c2c8e27
Steph Samson <sdvsamson@gmail.com>
2014-09-11 11:37:55 -0400

    Naive parser.

--- scheme.py
+++ scheme.py
@@ -1,14 +1,45 @@
+def read(user_input):
+  return parser(tokenize(user_input))
+
+def parser(tokens):
+  """
+  parser(tokenized) -> array (for now)
+
+  Parses the tokens and builds a (nested) array for
+  evaluation.
+
+  """
+  
+  parsed = []
+
+  # So that we can properly recurse
+  # and append a nested array
+  open_paren = tokens.pop(0)
+
+  while tokens:
+
+    token = tokens[0]
+    if token == '(':
+      parsed.append(parser(tokens))
+    elif token == ')':
+      tokens.pop(0)
+      return parsed
+    else:
+      parsed.append(tokens.pop(0))
+
+  return parsed
+    
 def tokenize(line):
   """
   tokenize(line) -> string
 
   Takes a string of Scheme code and breaks it up into
-  a series of tokens.
+  a series of tokens in a list.
   """
+  return line.replace('(',' ( ').replace(')',' ) ').split()
 
-  line = line.replace('(', ' ( ').replace(')', ' ) ').split(' ')
-  return [atom for atom in line if atom]
 
 if __name__ == '__main__':
 
-  print tokenize('((lambda(x) x "Lisp")')
+  scheme = raw_input()
+  print read(scheme)
