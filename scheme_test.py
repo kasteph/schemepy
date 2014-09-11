ca770d30b2ab4883dabc1eaf4380302dced0ee7a
Steph Samson <sdvsamson@gmail.com>
2014-09-11 11:39:31 -0400

    Moar tests.

--- scheme_test.py
+++ scheme_test.py
@@ -1,15 +1,25 @@
 import unittest
-from scheme import tokenize
+from scheme import tokenize, parser, read
+
 
 class TestScheme(unittest.TestCase):
 
-  def test_tokenize(self):
-    """
-    Test that a line of Scheme will be broken up into 'atoms' or tokens in a list.
-    """
+  line = '((lambda(x) x) "Lisp")'
 
-    line = tokenize('((lambda(x) x) "Lisp")')
-    self.assertEqual(line, ['(', '(', 'lambda', '(', 'x', ')', 'x', ')', '"Lisp"', ')'])
+  def test_tokenize(self):
+    self.assertEqual(tokenize(self.line), ['(', '(', 'lambda', '(', 'x', ')', 'x', ')', '"Lisp"', ')'])
+
+  def test_parser(self):
+    self.assertEqual(parser(tokenize(self.line)), [['lambda', ['x'], 'x'], '"Lisp"'])
+
+  def test_read_simple_expression(self):
+    self.assertEqual(read('(1 2)'), ['1', '2'])
+
+  def test_read_simple_lambda(self):
+    self.assertEqual(read(self.line), [['lambda', ['x'], 'x'], '"Lisp"'])
+
+
+
 
 
 if __name__ == '__main__':
