14f325ed32fb14a16d69b3698eaa473b133de4f8
Steph Samson <sdvsamson@gmail.com>
2014-09-16 12:16:43 -0400

    Add environment and its tests

--- scheme.py
+++ scheme.py
@@ -39,6 +39,23 @@ def tokenize(line):
   a series of tokens in a list.
   """
   return re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', line.replace('(',' ( ').replace(')',' ) ')  )
+class Environment(object):
+  def __init__(self, scopes):
+    self.scopes = scopes
+
+  def get(self, symbol):
+    for scope in self.scopes:
+      if symbol in scope:
+        return scope[symbol]
+
+  def set(self, symbol, new_val):
+    self.scopes[0][symbol] = new_val
+
+  def add_scope(self):
+    self.scopes = [{}] + self.scopes
+
+  def remove_scope(self):
+    self.scopes.pop(0)
 
 
 if __name__ == '__main__':
--- scheme_test.py
+++ scheme_test.py
@@ -1,5 +1,5 @@
 import unittest
-from scheme import tokenize, parser, read
+from scheme import tokenize, parser, read, Environment, eval
 
 
 class TestScheme(unittest.TestCase):
@@ -18,6 +18,22 @@ class TestScheme(unittest.TestCase):
   def test_read_simple_lambda(self):
     self.assertEqual(read(self.line), [['lambda', ['x'], 'x'], '"Lisp"'])
 
+  def test_environment(self):
+    a = Environment([{}])
+
+    self.assertEqual(a.get('x'), None)
+    
+    a.set('x', 1)
+    self.assertEqual(a.get('x'), 1)
+
+    a.add_scope()
+    a.set('x', 2)
+
+    self.assertEqual(a.get('x'), 2)
+
+    a.remove_scope()
+
+    self.assertEqual(a.get('x'), 1)
 
 
 
