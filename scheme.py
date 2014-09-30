fa0584b69d552f08fbbaa42fbe6427ca5da9915b
Steph Samson <sdvsamson@gmail.com>
2014-09-30 00:12:26 -0400

    REPL.

--- scheme.py
+++ scheme.py
@@ -1,13 +1,22 @@
+import sys
+
 from parser import read
 from evaluator import Evaluator
 
-def REP(line, evaluator):
-    return evaluator.eval(read(line))
+def REPL(line, evaluator):
+    try:
+        return evaluator.eval(read(line))
+    except IndexError:
+        return ''
 
 
 if __name__ == '__main__':
-
+    print 'Welcome to schemepy. Press <Ctrl-c> to exit.'
     e = Evaluator()
     while True:
-        scheme = raw_input()
-        print REP(scheme, e)
+        try:
+            scheme = raw_input()
+            print REPL(scheme, e)
+        except (KeyboardInterrupt, SystemExit):
+            print 'Exiting schemepy.'
+            sys.exit()
